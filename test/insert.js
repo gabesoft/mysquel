a64816ac7fed890dd06daf01e320005ff5bd2766
Gabriel Adomnicai <gabesoft@gmail.com>
2012-10-03 17:56:55 -0700

    more unit tests

--- test/insert.js
+++ test/insert.js
@@ -14,4 +14,17 @@ describe('insert', function() {
 
         sql.should.equal('INSERT INTO test (f1, f2, f3, f4, f5) VALUES (1, 1.2, TRUE, \'blah\', NULL) ON DUPLICATE KEY UPDATE f2=1.2, f4=\'blah\'');
     });
+
+    it('should generate insert statement without ON DUPLICATE option', function() {
+        var sql = squel.insert()
+               .into('test')
+               .set('f1', 1)
+               .set('f2', 1.2)
+               .set('f3', true)
+               .set('f4', 'blah')
+               .set('f5', null)
+               .toString();
+
+        sql.should.equal('INSERT INTO test (f1, f2, f3, f4, f5) VALUES (1, 1.2, TRUE, \'blah\', NULL)');
+    });
 });
